922dab37dec922bfce9283a1f316c65f3f6c598e
KuzmichevAlex <kuzmichevadata@gmail.com>
2020-10-22 13:36:18 +0300

    Initial Commit

---
+++ UIKit_Apps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKit_Apps
+//
+//  Created by Александр Кузьмичев on 22.10.2020.
+//  Copyright © 2020 Александр Кузьмичев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
